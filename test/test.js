198a6a86a1f086c9e0860ad5e83a9f7ef6ed6cd9
codexempire <princewillifeanyi1999@gmail.com>
2019-01-31 16:54:06 +0100

    test

--- test/test.js
+++ test/test.js
@@ -3,26 +3,6 @@ import { expect } from 'chai';
 
 import app from '../app';
 
-
-describe('GET /party',() => {
-  it('respond with a status code 200', (done) => {
-    request(app)
-      .get('/api/v1/party')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-  });
-
-  it('respond with an array of party object', (done) => {
-    request(app)
-      .get('/api/v1/party')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        expect(res.body.data).to.be.a("Array");
-        expect(res.body.data[0]).to.have.property('name');
-        expect(res.body.data[0]).to.have.property('logoUrl');
-        done(err);
-      });
-  });
-});
+describe('GET /party' () => {
+  it ('should return a 200 status message', )
+})
