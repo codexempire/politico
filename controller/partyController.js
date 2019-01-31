d67fdc34fe05c14099e215575e57c29265e0ee17
codexempire <princewillifeanyi1999@gmail.com>
2019-01-31 16:46:25 +0100

    Saving updates

--- controller/partyController.js
+++ controller/partyController.js
@@ -1,6 +1,23 @@
 import PartyModel from '../model/partyModel';
 
 class PartyController {
+    static createParty(req, res) {
+    PartyMiddleware.createPartyCheck(req);
+    
+    if (req.error) {
+      res.status(404).json({
+        status: 404,
+        error: 'Provide name, address and logo of the party',
+      });
+    } else {
+      const { name, hqAddress, logoUrl } = req.body;
+      const id = PartyModel.createParty(name, hqAddress, logoUrl);
+      res.status(201).json({
+        status: 201,
+        data: [id, name,],
+      });
+    }
+  }
   static getAllParty(req, res) {
     const parties = PartyModel.getAllParty(res);
 
